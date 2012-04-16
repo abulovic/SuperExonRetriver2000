c6f6430d517adafc34ce7866fd9cf5c0d1ac442a
Mario Ogrizek-Tomaš <mario.guardian@gmail.com>
2012-04-16 16:24:50 +0800

    Added error handling in Alignments.py TODO: logging of these errors

--- ExoLocator/pipeline/alignment/Alignments.py
+++ ExoLocator/pipeline/alignment/Alignments.py
@@ -5,9 +5,9 @@ Created on Apr 13, 2012
 '''
 
 from pipeline.utilities.DirectoryCrawler import DirectoryCrawler
-from pipeline.alignment.AlignmentCommandGenerator import AlignmentCommandGenerator
+from pipeline.utilities.CommandGenerator import CommandGenerator
 from pipeline.alignment.AlignmentTargetGenerator  import AlignmentTargetGenerator
-import subprocess
+from subprocess import Popen, PIPE, STDOUT
 import os
 
 def generate_blastn_alignments(protein_id, species_list = None):
@@ -21,14 +21,22 @@ def generate_blastn_alignments(protein_id, species_list = None):
         species_list    = AlignmentTargetGenerator().get_blastn_targets(protein_id)
     
     crawler             = DirectoryCrawler()
-    command_generator   = AlignmentCommandGenerator()
+    command_generator   = CommandGenerator()
     for species in species_list:
         output_file     = "{0}/{1}.blastout".format(crawler.get_blastn_path(protein_id), species.strip())
         input_file      = "{0}/{1}.fa".format(crawler.get_expanded_gene_path(protein_id), species.strip())
         database        = "{0}/Homo_sapiens.fa".format(crawler.get_exon_ensembl_path(protein_id))
         
-        #subprocess.call(command_generator.generate_blastn_command(database, input_file, output_file))
-         
+        cmd = command_generator.generate_blastn_command(database, input_file, output_file)
+        print cmd
+        p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+        output = p.stdout.read()
+        if output != "":
+#TODO: LOGGING
+            print output
+           
+        
+
 def generate_tblastn_alignments(protein_id, species_list = None):
     '''
         Runs the tblastn program for a specified protein and list of species
@@ -40,14 +48,20 @@ def generate_tblastn_alignments(protein_id, species_list = None):
         species_list    = AlignmentTargetGenerator().get_tblastn_targets(protein_id)
     
     crawler             = DirectoryCrawler()
-    command_generator   = AlignmentCommandGenerator()
+    command_generator   = CommandGenerator()
     for species in species_list:
         output_file     = "{0}/{1}.blastout".format(crawler.get_tblastn_path(protein_id), species.strip())
         input_file      = "{0}/{1}.fa".format(crawler.get_protein_path(protein_id), species.strip())
         database        = "{0}/Homo_sapiens.fa".format(crawler.get_exon_ensembl_path(protein_id))
         
-        #subprocess.call(command_generator.generate_tblastn_command(database, input_file, output_file))
-        
+        cmd = command_generator.generate_tblastn_command(database, input_file, output_file)
+        print cmd
+        p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+        output = p.stdout.read()
+        if output != "":
+#TODO: LOGGING
+            print output
+            
 def generate_SW_gene_alignments(protein_id, species_list = None):
     '''
         Runs the SW program for a specified protein and list of species, using the expanded gene region.
@@ -58,14 +72,19 @@ def generate_SW_gene_alignments(protein_id, species_list = None):
     if (not species_list):
         species_list         = AlignmentTargetGenerator().get_SW_gene_targets(protein_id)
     crawler                  = DirectoryCrawler()
-    command_generator        = AlignmentCommandGenerator()
+    command_generator        = CommandGenerator()
     for species in species_list:
         output_file          = "{0}/{1}.swout".format(crawler.get_SW_gene_path(protein_id), species.strip())
         query_sequence_file  = "{0}/{1}.fa".format(crawler.get_expanded_gene_path(protein_id), species.strip())
         target_fasta_db_file = "{0}/Homo_sapiens.fa".format(crawler.get_exon_ensembl_path(protein_id))
         
-        #subprocess.call(command_generator.generate_SW_command(query_sequence_file, target_fasta_db_file, output_file))
-            
+        cmd = command_generator.generate_SW_command(query_sequence_file, target_fasta_db_file, output_file)
+        print cmd
+        p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+        output = p.stdout.read()
+        if output != "":
+#TODO: LOGGING
+            print output
     os.remove(".sw_stdout_supressed")
         
 def generate_SW_exon_alignments(protein_id, species_list = None):
@@ -79,13 +98,18 @@ def generate_SW_exon_alignments(protein_id, species_list = None):
         species_list    = AlignmentTargetGenerator().get_SW_exon_targets(protein_id)
     
     crawler             = DirectoryCrawler()
-    command_generator   = AlignmentCommandGenerator()
+    command_generator   = CommandGenerator()
     for species in species_list:
         ensembl_exons_path  = "{0}/{1}.fa".format(crawler.get_exon_ensembl_path(protein_id), species.strip())
         target_fasta_db_file = "{0}/Homo_sapiens.fa".format(crawler.get_exon_ensembl_path(protein_id))
         # Isolate each exon to a temporary file to be processed with SW
         tmp_file = "tmp_sw.fa"
-        ensembl_exons_file = open(ensembl_exons_path, 'r')
+        try:
+            ensembl_exons_file = open(ensembl_exons_path, 'r')
+        except IOError, e:
+#TODO: LOGGING
+            print e
+            continue
         exon_seq = ""
         exon_header = ""
         exon_counter = 0
@@ -99,8 +123,13 @@ def generate_SW_exon_alignments(protein_id, species_list = None):
                     tmp.write("{0}\n{1}\n".format(exon_header, exon_seq))
                     tmp.close()
                     
-                    #subprocess.call(command_generator.generate_SW_command(tmp_file, target_fasta_db_file, output_file))
-                    print command_generator.generate_SW_command(tmp_file, target_fasta_db_file, output_file)
+                    cmd = command_generator.generate_SW_command(tmp_file, target_fasta_db_file, output_file)
+                    print cmd
+                    p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+                    output = p.stdout.read()
+                    if output != "":
+#TODO: LOGGING
+                        print output
                 exon_header = line.strip()
                 exon_seq = ""
             else:
@@ -113,13 +142,22 @@ def generate_SW_exon_alignments(protein_id, species_list = None):
         tmp.write("{0}\n{1}\n".format(exon_header, exon_seq))
         tmp.close()
         
-        #subprocess.call(command_generator.generate_SW_command(tmp_file, target_fasta_db_file, output_file))
-        print command_generator.generate_SW_command(tmp_file, target_fasta_db_file, output_file)
+        cmd = command_generator.generate_SW_command(tmp_file, target_fasta_db_file, output_file)
+        print cmd
+        p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+        output = p.stdout.read()
+        if output != "":
+#TODO: LOGGING
+            print output
+            
         ensembl_exons_file.close()
         os.remove(tmp_file)
         os.remove(".sw_stdout_supressed")
   
 def main ():
+    generate_blastn_alignments("ENSP00000311134")
+    generate_tblastn_alignments("ENSP00000311134")
+    generate_SW_gene_alignments("ENSP00000311134")
     generate_SW_exon_alignments("ENSP00000311134")
     
 if __name__ == '__main__':
