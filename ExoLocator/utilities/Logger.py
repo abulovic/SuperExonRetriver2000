88143784bfb523ac34e2dd8129f68080ab0040af
Mario Ogrizek-Tomaš <mario.guardian@gmail.com>
2012-04-16 10:31:58 +0800

    Renamed folders.

---
+++ ExoLocator/utilities/Logger.py
@@ -0,0 +1,39 @@
+'''
+Created on Apr 15, 2012
+
+@author: intern
+'''
+
+import logging
+import logging.config
+import sys, re
+
+class Logger(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        
+        cfg_file = self.get_cfg_dir()+"/logging.cfg"
+        logging.config.fileConfig(cfg_file)
+        
+    def get_logger (self, logger_name):
+        return logging.getLogger(logger_name)
+        
+    def get_cfg_dir(self):
+        '''
+        Auxiliary function, gets the configuration directory
+        '''
+        ex_path = sys.path[0]
+        m = re.match("(.*ExoLocator).*", ex_path)
+        proj_root_dir = m.groups()[0]
+        return proj_root_dir + "/cfg/"
+        
+    
+        
+if __name__ == '__main__':
+    logger = Logger()
+    l = logging.getLogger('mutual_best')
+    l.debug('tralala')
